623956219172792773e1ad8e350b8a8295dee405
syalam <sheehan@stocktwits.com>
2011-12-14 09:38:10 +0000

    big merge with richards refactored codebase and my code

---
+++ src/com/sonrlabs/test/sonr/common/DialogCommon.java
@@ -0,0 +1,23 @@
+package com.sonrlabs.test.sonr.common;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class DialogCommon {
+
+    private static DialogInterface.OnClickListener doNothingListener = new DialogInterface.OnClickListener() {
+	public void onClick(DialogInterface dialog, int id) {
+	    //do nothing
+	}
+    };
+    
+    public static void quickPopoutDialog(Context c, boolean cancellable, String msg, String buttonText) {
+	AlertDialog.Builder builder = new AlertDialog.Builder(c);
+	builder.setMessage(msg);
+	builder.setCancelable(cancellable);
+	builder.setNeutralButton(buttonText, doNothingListener);
+	builder.create().show();
+    }
+    
+}

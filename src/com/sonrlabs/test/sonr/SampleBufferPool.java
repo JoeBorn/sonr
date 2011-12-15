735bb9a9e8028c97a263c75a8be7158230e79911
R Shapiro <rshapiro@bbn.com>
2011-12-15 12:48:24 -0500

    Fix copyright notice and remove the bogus ones using the wrong text.

--- src/com/sonrlabs/test/sonr/SampleBufferPool.java
+++ src/com/sonrlabs/test/sonr/SampleBufferPool.java
@@ -1,25 +1,3 @@
-/***************************************************************************
- *
- * <rrl>
- * =========================================================================
- *                                  LEGEND
- *
- * Use, duplication, or disclosure by the Government is as set forth in the
- * Rights in technical data noncommercial items clause DFAR 252.227-7013 and
- * Rights in noncommercial computer software and noncommercial computer
- * software documentation clause DFAR 252.227-7014, with the exception of
- * third party software known as Sun Microsystems' Java Runtime Environment
- * (JRE), Quest Software's JClass, Oracle's JDBC, and JGoodies which are
- * separately governed under their commercial licenses.  Refer to the
- * license directory for information regarding the open source packages used
- * by this software.
- *
- * Copyright 2011 by BBN Technologies Corporation.
- * =========================================================================
- * </rrl>
- *
- **************************************************************************/
-
 package com.sonrlabs.test.sonr;
 
 import java.util.ArrayList;

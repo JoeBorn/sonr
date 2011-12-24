f8c64b46289ca3fd23498db697af7bff250d0547
syalam <sheehan@stocktwits.com>
2011-12-24 14:23:36 -0700

    tweaked deploy script to fetch from master before building from source

--- deploy-sonr.sh
+++ deploy-sonr.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+#get the latest source
+git pull origin master
+
+#build and sign from source
 ant release
+
+#install signed apk on all phones
 sh install-sonr.sh
--- install-sonr.sh
+++ install-sonr.sh
@@ -2,7 +2,10 @@
 
 #install SONR
 echo "installing latest build of SONR"
+echo "--------------------------------"
 for foo in `adb devices | egrep 'device$' | cut -d ' ' -f1`
 do
+	echo "Device: ${foo}"
     adb -s $foo install -r $PWD/bin/SONR-release.apk
 done
+echo "finished installing SONR"

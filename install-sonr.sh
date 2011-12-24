23384fea2177104c705abf464d0392a95736476b
syalam <sheehan@stocktwits.com>
2011-12-24 14:39:38 -0700

    added a counter to keep track of how many devices SONR has been deployed on

--- install-sonr.sh
+++ install-sonr.sh
@@ -3,9 +3,11 @@
 #install SONR
 echo "installing latest build of SONR"
 echo "--------------------------------"
+i=0
 for foo in `adb devices | egrep 'device$' | cut -d ' ' -f1`
 do
 	echo "Device: ${foo}"
     adb -s $foo install -r $PWD/bin/SONR-release.apk
+    let i+=1
 done
-echo "finished installing SONR"
+echo "SONR deployed to ${i} devices"

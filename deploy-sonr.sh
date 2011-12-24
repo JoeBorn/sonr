8b42231dd0fa041cf3f262df776cee30c641f69f
syalam <sheehan@stocktwits.com>
2011-12-24 10:51:27 -0700

    deploy script that makes it much easier to put SONR on several phones

---
+++ deploy-sonr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#install SONR
+echo "installing latest build of SONR"
+for foo in `adb devices | egrep 'device$' | cut -d ' ' -f1`
+do
+    adb -s $foo install -r $PWD/bin/SONR.apk
+done

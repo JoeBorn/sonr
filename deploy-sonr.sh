46f631610cff3f69a1d8e1d98b16c35b37653d79
syalam <sheehan@stocktwits.com>
2011-12-24 11:57:26 -0700

    moved keystore into main dir so build.xml can find it, added some deploy scripts to automate building and installing

--- deploy-sonr.sh
+++ deploy-sonr.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-#install SONR
-echo "installing latest build of SONR"
-for foo in `adb devices | egrep 'device$' | cut -d ' ' -f1`
-do
-    adb -s $foo install -r $PWD/bin/SONR.apk
-done
+ant release
+sh install-sonr.sh

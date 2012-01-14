bb01ee18b7422a08f3abfdf6a73a58e3b48f0696
R Shapiro <rshapiro@comcast.net>
2012-01-14 14:48:18 -0500

    More signal logging

--- src/com/sonrlabs/test/sonr/signal/SignalConstructor.java
+++ src/com/sonrlabs/test/sonr/signal/SignalConstructor.java
@@ -1,15 +1,19 @@
 package com.sonrlabs.test.sonr.signal;
 
+import android.util.Log;
+
 import com.sonrlabs.test.sonr.AudioProcessorQueue;
 
 abstract class SignalConstructor
       implements AudioSupportConstants {
 
+   private static final String TAG = "SignalConstructor";
+   private static final int MIN_MATCHES = 2;
    private int signalMaxSum = 0;
    private final int[] movingbuf = new int[MOVING_SIZE];
    private final int[] movingsum = new int[TRANSMISSION_LENGTH];
    private final int[] signals = new int[SAMPLES_PER_BUFFER];
-   
+
    /**
     * The use case for this method is {@link #constructSignal(short[], int[])}.
     * That method is invoked by both by {@link ListenerAudioSupport} and also by
@@ -29,31 +33,30 @@ abstract class SignalConstructor
     * @return whether or not signal construction should proceeed.
     */
    abstract protected boolean checkSignal(int signal);
-   
-   boolean processSignalIfMatch(int firstIndex, int secondIndex)
+
+   void processSignalIfMatch()
          throws SpuriousSignalException {
-      int first = signals[firstIndex];
-      boolean match = first != 0 & first != BOUNDARY && first == signals[secondIndex];
-      if (match) {
-         AudioProcessorQueue.processAction(first);
+      for (int i = 0; i < signals.length; i++) {
+         int baseSignal = signals[i];
+         if (baseSignal != 0 && baseSignal != BOUNDARY) {
+            int matchCount = 1;
+            for (int j = i + 1; j < signals.length; j++) {
+               if (baseSignal == signals[j]) {
+                  ++matchCount;
+               }
+            }
+            if (matchCount >= MIN_MATCHES) {
+               AudioProcessorQueue.processAction(baseSignal);
+               String msg = "Detected signal " 
+                     + Integer.toBinaryString(baseSignal) + " [0x" + Integer.toHexString(baseSignal) + "] with "
+                     + matchCount + " matches";
+               Log.d(TAG, msg);
+               return;
+            }
+         }
       }
-      return match;
    }
-   
-   String generateLog(int samplelocsize, short[] samples, int[] sampleStartIndices, int signalCode) {
-      StringBuilder builder = new StringBuilder();
-      builder.append("Spurious signal ").append(signalCode).append(" sample-count=").append(samplelocsize);
-      builder.append(" Signals:");
-      for (int i=0; i<signals.length; i++) {
-         builder.append(" ").append(i).append('=').append(signals[i]);
-      }
-      builder.append(" Indices:");
-      for (int i=0; i<sampleStartIndices.length; i++) {
-         builder.append(" ").append(i).append('=').append(sampleStartIndices[i]).append(" ->" ).append(samples[i]);
-      }
-      return builder.toString();
-   }
-   
+
    int countBoundSignals() {
       int matchCount = 0;
       for (int value : signals) {
@@ -63,13 +66,13 @@ abstract class SignalConstructor
       }
       return matchCount;
    }
-   
+
    boolean isPhaseChange(int movingSumIndex) {
       int sum1 = movingsum[movingSumIndex];
-      int sum2 = movingsum[movingSumIndex+1];
+      int sum2 = movingsum[movingSumIndex + 1];
       return Math.abs(sum1 - sum2) > signalMaxSum;
    }
-   
+
    int findSample(int startpos, short[] samples, int numsampleloc, int[] sampleStartIndices) {
       movingsum[0] = 0;
       int start = startpos + MOVING_SIZE;
@@ -87,7 +90,7 @@ abstract class SignalConstructor
          if (arraypos == MOVING_SIZE) {
             arraypos = 0;
          }
-   
+
          if (isPhaseChange(0)) {
             sampleStartIndices[numsampleloc++] = i - 5;
             if (numsampleloc >= SAMPLES_PER_BUFFER) {
@@ -106,7 +109,7 @@ abstract class SignalConstructor
       }
       return numsampleloc;
    }
-   
+
    void computeSignalMax(short[] samples, int startpos) {
       movingsum[0] = 0;
       for (int i = startpos; i < startpos + MOVING_SIZE; i++) {
@@ -124,15 +127,15 @@ abstract class SignalConstructor
          if (index == MOVING_SIZE) {
             index = 0;
          }
-   
+
          int temp = Math.abs(movingsum[0] - movingsum[1]);
          if (temp > signalMaxSum) {
             signalMaxSum = temp;
          }
-   
+
          movingsum[0] = movingsum[1];
       }
-   
+
       signalMaxSum /= 1.375;
    }
 
@@ -156,7 +159,7 @@ abstract class SignalConstructor
                   index = 0;
                }
             }
-   
+
             constructSignal(signalIndex);
          }
       }
@@ -166,13 +169,13 @@ abstract class SignalConstructor
       /* we start out with a phase shift and 0 signal */
       boolean inphase = true, switchphase = true;
       signals[signalIndex] = 0;
-   
-      for (int i = FRAMES_PER_BIT + 1, bitnum=0; i < TRANSMISSION_LENGTH; i++) {
-         if (switchphase && isPhaseChange(i-1)) {
+
+      for (int i = FRAMES_PER_BIT + 1, bitnum = 0; i < TRANSMISSION_LENGTH; i++) {
+         if (switchphase && isPhaseChange(i - 1)) {
             inphase = !inphase;
             switchphase = false;
          }
-   
+
          boolean atFrameBoundary = i % FRAMES_PER_BIT == 0;
          if (atFrameBoundary) {
             if (!inphase) {
--- src/com/sonrlabs/test/sonr/signal/TransmissionFinder.java
+++ src/com/sonrlabs/test/sonr/signal/TransmissionFinder.java
@@ -40,27 +40,11 @@ final class TransmissionFinder
       }
       constructSignal(samples, sampleStartIndices);
       try {
-         processSignal();
+         processSignalIfMatch();
       } catch (SpuriousSignalException e) {
-         String message = generateLog(samplelocsize, samples, sampleStartIndices, e.getSignalCode());
-         Log.d("Spurious Signal", message);
+         Log.d("TransmissionFinder", "Spurious Signal");
       }
    }
-
-   /*
-    * Look for two out of three match.
-    * 
-    * Works by side-effect which is why the bodies of the if statements are empty.
-    * Note the we're depending on SAMPLES_PER_BUFFER being 3 here.
-    */
-   private void processSignal()
-         throws SpuriousSignalException {
-      if (processSignalIfMatch(0, 1)) {
-      } else if (samplelocsize > 2 && processSignalIfMatch(0, 2)) {
-      } else if (samplelocsize > 2 && processSignalIfMatch(1, 2)) {
-      }
-   }
-   
 
    private void autoGainControl(short[] samples, int[] sampleStartIndices) {
       computeSignalMax(samples, sampleStartIndices[0]);

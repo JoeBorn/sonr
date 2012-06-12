0453ecabd3d7c50e75af50d6177285ddf03567b2
JoeBorn <joeborn@gmail.com>
2012-06-11 22:11:45 -0500

    added gpl 2 header to all files.

--- src/com/sonrlabs/test/sonr/SONRClient.java
+++ src/com/sonrlabs/test/sonr/SONRClient.java
@@ -1,3 +1,11 @@
+/***************************************************************************
+ * Copyright (c) 2011, 2012 by Sonr Labs Inc (http://www.sonrlabs.com)
+ *
+ *You can redistribute this program and/or modify it under the terms of the GNU General Public License v. 2.0 as published by the Free Software Foundation
+ *This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ **************************************************************************/
 package com.sonrlabs.test.sonr;
 
 //import org.acra.ErrorReporter;
